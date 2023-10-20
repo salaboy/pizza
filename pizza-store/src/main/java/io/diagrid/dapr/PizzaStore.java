aef709af34e4d97df96975da25429e1000f38181
salaboy <Salaboy@gmail.com>
2023-10-20 11:32:33 +0100

    adding tests following open api

--- pizza-store/src/main/java/io/diagrid/dapr/PizzaStore.java
+++ pizza-store/src/main/java/io/diagrid/dapr/PizzaStore.java
@@ -1,8 +1,11 @@
 package io.diagrid.dapr;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,47 +18,57 @@ import io.dapr.client.domain.State;
 @RestController
 public class PizzaStore {
 
+
   public static void main(String[] args) {
     SpringApplication.run(PizzaStore.class, args);
 
   }
 
  private String STATE_STORE_NAME = "statestore";
- private String ORDER = "pizza";
+ private String ORDERS = "orders";
 
-  @PostMapping("/")
-  public String newPizza(@RequestBody Pizza pizza){
-
+  @PostMapping("/order")
+  public ResponseEntity<String> placeOrder(@RequestBody(required = true) Order order){
+    
     try (DaprClient client = (new DaprClientBuilder()).build()) {
         
         // Save state
-        client.saveState(STATE_STORE_NAME, ORDER, pizza).block();
-        return "Done!";
+        client.saveState(STATE_STORE_NAME, ORDERS, order).block();
+        return ResponseEntity.ok("Done!");
     }catch(Exception ex){
         ex.printStackTrace();
-        return "Error!";
+        return ResponseEntity.internalServerError().body("Error placing the order! Check the logs.");
     }
-    
 
   }
 
-  @GetMapping("/")
-  public Pizza getPizza(){
+  // @GetMapping("/")
+  // public List<Order> getOrders(){
 
-    try (DaprClient client = (new DaprClientBuilder()).build()) {
+  //   try (DaprClient client = (new DaprClientBuilder()).build()) {
        
 
-        // Get state
-        State<Pizza> retrievedMessage = client.getState(STATE_STORE_NAME, ORDER, Pizza.class).block();
+  //       // Get state
+  //       State<Order> retrievedMessage = client.getState(STATE_STORE_NAME, ORDER, Order.class).block();
 
-        return retrievedMessage.getValue();
+  //       return retrievedMessage.getValue();
 
-    }catch(Exception ex){
-        ex.printStackTrace();
+  //   }catch(Exception ex){
+  //       ex.printStackTrace();
+  //   }
+  //   return null;
+  // }
+
+  public record Customer(String name, String email){}
+  public record OrderItem(PizzaType type, int amount){}
+  public enum PizzaType{pepperoni, margherita, hawaiian, vegetarian}
+  public enum Status{placed, instock, notinstock,inpreparation,completed}
+  public record Order(String id, Customer customer, List<OrderItem> items, Date orderDate, Status status){
+    public Order(Customer customer, List<OrderItem> items, Date orderDate, Status status) {
+      this(UUID.randomUUID().toString(), customer, items, orderDate, status);
     }
-    return null;
   }
 
-public record Pizza (String type, String size) {}
-
 }
+
+
--- pizza-store/src/test/java/io/diagrid/dapr/PizzaStoreTest.java
+++ pizza-store/src/test/java/io/diagrid/dapr/PizzaStoreTest.java
@@ -1,20 +1,50 @@
 package io.diagrid.dapr;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import io.diagrid.dapr.PizzaStore.Customer;
+import io.diagrid.dapr.PizzaStore.Order;
+import io.diagrid.dapr.PizzaStore.OrderItem;
+import io.diagrid.dapr.PizzaStore.PizzaType;
+import io.diagrid.dapr.PizzaStore.Status;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
+import static io.restassured.RestAssured.with;
+
+import java.util.Arrays;
+import java.util.Date;
+
+@SpringBootTest(classes=PizzaStoreAppTest.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public class PizzaStoreTest {
-    public static void main(String[] args) {
-        SpringApplication.from(PizzaStore::main)
-                .with(DaprTestConfiguration.class)
-                .run(args);
-    }
 
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Setting up!");
+        RestAssured.port = port;
+    }
     
-    @ImportTestcontainers(DaprLocal.class)
-    static class DaprTestConfiguration {
-       
+    @Test
+    public void testPlaceOrder() throws Exception {
+        
+        with().body(new Order(new Customer("salaboy", "salaboy@mail.com"), 
+                                Arrays.asList(new OrderItem(PizzaType.pepperoni, 1)), 
+                                new Date(), Status.placed ))
+                                .contentType(ContentType.JSON)
+        .when()
+        .request("POST", "/order")
+        .then()
+        .statusCode(200);
     }
 
 }
