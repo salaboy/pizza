dafaae0a9f0d4db03a5175f73560f2e04111cf3c
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-01-29 16:27:18 +0100

    Add Microcks contract testing + update to latest Dapr and Spring Boot Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- pizza-kitchen/src/test/java/io/diagrid/dapr/PizzaKitchenAppTest.java
+++ pizza-kitchen/src/test/java/io/diagrid/dapr/PizzaKitchenAppTest.java
@@ -2,22 +2,13 @@ package io.diagrid.dapr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-
-import io.diagrid.dapr.profiles.DaprBasicProfile;
 
 @SpringBootApplication
 public class PizzaKitchenAppTest {
+
     public static void main(String[] args) {
         SpringApplication.from(PizzaKitchen::main)
-                .with(DaprTestConfiguration.class)
+                .with(DaprTestContainersConfig.class)
                 .run(args);
     }
-
-    
-    @ImportTestcontainers(DaprBasicProfile.class)
-    static class DaprTestConfiguration {
-       
-    }
-
 }
--- pizza-kitchen/src/test/java/io/diagrid/dapr/PizzaKitchenTest.java
+++ pizza-kitchen/src/test/java/io/diagrid/dapr/PizzaKitchenTest.java
@@ -3,7 +3,7 @@ package io.diagrid.dapr;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.context.annotation.Import;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -22,15 +22,19 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.*;
 
 @SpringBootTest(classes=PizzaKitchenAppTest.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@Testcontainers
-public class PizzaKitchenTest {
+@Import(DaprTestContainersConfig.class)
+class PizzaKitchenTest {
 
     @Autowired
     private SubscriptionsRestController subscriptionsRestController;
 
     
     @Test
-    public void testPrepareOrderRequest() throws Exception {
+    void testPrepareOrderRequest() throws Exception {
+        // Get initial events size - it may not be empty if we're running in a test suite.
+        List<CloudEvent<Event>> events = subscriptionsRestController.getAllEvents();
+        int eventsSize = events.size();
+
         with().body(new Order(UUID.randomUUID().toString(), 
                                 Arrays.asList(new OrderItem(PizzaType.pepperoni, 1)), 
                                 new Date()))
@@ -40,12 +44,12 @@ public class PizzaKitchenTest {
         .then().assertThat().statusCode(200);
 
         // Wait for the event to arrive
-        Thread.sleep(17000);
+        Thread.sleep(20000);
 
-        List<CloudEvent<Event>> events = subscriptionsRestController.getAllEvents();
-        assertEquals("Two published event are expected",2, events.size());
-        assertEquals("The content of the cloud event should be the in preparation event", EventType.ORDER_IN_PREPARATION, events.get(0).getData().type());
-        assertEquals("The content of the cloud event should be the ready event", EventType.ORDER_READY, events.get(1).getData().type());
+        events = subscriptionsRestController.getAllEvents();
+        assertEquals("Two published event are expected",2, events.size() - eventsSize);
+        assertEquals("The content of the cloud event should be the in preparation event", EventType.ORDER_IN_PREPARATION, events.get(eventsSize).getData().type());
+        assertEquals("The content of the cloud event should be the ready event", EventType.ORDER_READY, events.get(eventsSize + 1).getData().type());
 
     }
 
