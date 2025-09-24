bd90f1b44c5752aae1a24ea39fc2b1be33cfa639
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-09-24 17:09:17 +0200

    ci: Try to initialize host access earlier to avoid race condition Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- pizza-kitchen/src/test/java/com/salaboy/pizza/kitchen/DaprTestContainersConfig.java
+++ pizza-kitchen/src/test/java/com/salaboy/pizza/kitchen/DaprTestContainersConfig.java
@@ -84,6 +84,12 @@ public class DaprTestContainersConfig {
             .withLogConsumer(outputFrame -> System.out.println(outputFrame.getUtf8String()))
             .dependsOn(kafkaContainer);
 
+        // Necessary to access app HTTP endpoint from tests.
+        boolean mocks = env.getProperty("tests.mocks", Boolean.class, false);
+        if (mocks) {
+            org.testcontainers.Testcontainers.exposeHostPorts(8081);
+        }
+
         return daprContainer;
     }
 
@@ -101,9 +107,6 @@ public class DaprTestContainersConfig {
     @Bean
     @ConditionalOnProperty(prefix = "tests", name = "mocks", havingValue = "true")
     MicrocksContainersEnsemble microcksEnsemble(Network network) {
-        // Necessary to access app HTTP endpoint from tests.
-        org.testcontainers.Testcontainers.exposeHostPorts(8081);
-
         return new MicrocksContainersEnsemble(network, "quay.io/microcks/microcks-uber:1.12.1-native")
             .withAsyncFeature()
             .withAccessToHost(true)
